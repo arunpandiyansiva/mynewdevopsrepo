bf74eabe5b70ff185c89385655fb8d9cff574bd4
arunpandiyansiva <arunpandiyan.siva@gmail.com>
2023-05-12 07:23:13 +0530

    Modified with new changes

--- login.java
+++ login.java
@@ -9,4 +9,11 @@ edit some changes
 again
 thank you
 welcome to Java
+what is the changes of window B
+
+and then again I need to commit 
+the changes
+Welcome 
+to second window
+
 >>>>>>> 297731b740e6bbf4bc21f10cb98d20ebe3f36db8
